d0d9f408ea30995ed65bd8df986c4f32c15d6e69
David Fischer <david@konst.fish>
2019-04-14 20:05:33 +0200

    Initial Commit

---
+++ Coral/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Coral
+//
+//  Created by David Fischer on 14.04.19.
+//  Copyright © 2019 fISH. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
