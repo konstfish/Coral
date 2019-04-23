5957d870cd97cf4beff6beaf4c2c3d83f82b0208
David Fischer <david.fishical@gmail.com>
2019-04-23 14:56:00 +0200

    add readme, fix code

--- Coral/AppDelegate.swift
+++ Coral/AppDelegate.swift
@@ -10,17 +10,22 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    let vc = ViewController ()
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
 
+    func applicationShouldOpenUntitledFile(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    @IBAction func itemTest(_ sender: Any) {
+        print("test")
+    }
+    
 }
 
