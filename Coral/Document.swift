f237f3bf9948f0ee92422216891cf661c507076d
David Fischer <david@konst.fish>
2019-04-23 14:56:00 +0200

    add readme, fix code

--- Coral/Document.swift
+++ Coral/Document.swift
@@ -7,16 +7,19 @@
 //
 
 import Cocoa
+import Quartz
 
 class Document: NSDocument {
 
+    var pdfdoc: PDFDocument?
+    
     override init() {
         super.init()
         // Add your subclass-specific initialization here.
     }
 
     override class var autosavesInPlace: Bool {
-        return true
+        return false
     }
 
     override func makeWindowControllers() {
@@ -33,10 +36,10 @@ class Document: NSDocument {
     }
 
     override func read(from data: Data, ofType typeName: String) throws {
-        // Insert code here to read your document from the given data of the specified type, throwing an error in case of failure.
-        // Alternatively, you could remove this method and override read(from:ofType:) instead.
-        // If you do, you should also override isEntireFileLoaded to return false if the contents are lazily loaded.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        self.pdfdoc = PDFDocument.init(data: data)
+        if self.pdfdoc == nil {
+            throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        }
     }
 
 
--- Coral/ViewController.swift
+++ Coral/ViewController.swift
@@ -7,21 +7,36 @@
 //
 
 import Cocoa
+import Quartz
 
 class ViewController: NSViewController {
 
+    
+    @IBOutlet var pdfview: PDFView!
+    @IBOutlet var thumbs: PDFThumbnailView!
+    @IBOutlet var thumb_view: NSView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewDidAppear() {
+        let document = self.view.window?.windowController?.document as! Document
+        self.view.window?.title = document.displayName
+        self.pdfview.document = document.pdfdoc
+        self.pdfview.autoScales = true
+        self.pdfview.acceptsDraggedFiles = true
+        self.pdfview.backgroundColor = .init(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0)
+        self.pdfview.pageShadowsEnabled = true
+        self.thumbs.pdfView = self.pdfview
+        self.thumb_view.isHidden = true
+    }
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
         }
     }
-
-
 }
 
